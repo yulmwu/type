f2449d253a26358ae35e825d55ae3a4a09ef7588
Rusty cat <tsukiroku@yahoo.com>
2022-04-04 21:24:20 +0900

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -5,16 +5,31 @@ type Split<T extends string, S extends string = ""> =
       ? [A, ...Split<B, S>]
       : [T];
 
-type Arr<N extends number, V extends Array<number> = []> =
-  V["length"] extends N
-    ? V
-    : Arr<N, [...V, 0]>;
-  
+type Arr<N extends number, V extends Array<number> = []> = 
+    V extends { length: N } 
+        ? V : Arr<N, [...V, 0]>;
+
 type Add<A extends number, B extends number> = 
-  [...Arr<A>, ...Arr<B>] extends { length: infer L }
-    ? L extends number    
-      ? L
-      : never
-    : never;
+    [...Arr<A>, ...Arr<B>] extends { length: infer L }
+        ? L : never;
+
+type Sub<A extends number, B extends number> =
+    Arr<A> extends [...Arr<B>, ...infer R]
+        ? R extends { length: infer L } ? L : never 
+            : never;
+
+type Mul<A extends number, B extends number> =
+    A extends 0 
+        ? 0 : B extends 0
+            ? 0 : B extends 1
+                // @ts-ignore
+                ? A : Add<Mul<A, Sub<B, 1>>, A>;
+
+type Div<A extends number, B extends number> = 
+    A extends 0 
+        ? 0 : B extends 0
+            ? never : Sub<A, B> extends never
+                ? 0 : Add<1, Div<Sub<A, B> extends never
+                    ? 0 : Sub<A, B>, B>>;
   
-declare export type { Split, Add };
+declare export type { Split, Add, Sub, Mul, Div };
