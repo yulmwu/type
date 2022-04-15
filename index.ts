ca54eff08c81d1b6dad93680e4cee25c6afd5571
Rusty cat <tsukiroku@yahoo.com>
2022-04-15 17:37:44 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,7 +3,8 @@ import type {
     Add,
     Sub,
     Mul,
-    Div
+    Div,
+    Pow
 } from './src';
 
 type split = Split<"hello world">; // ["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"]
@@ -11,3 +12,4 @@ type add = Add<1, 2>; // 3
 type sub = Sub<3, 2>; // 1
 type mul = Mul<3, 2>; // 6
 type div = Div<6, 2>; // 3
+type pow = Pow<3, 5>; // 243
