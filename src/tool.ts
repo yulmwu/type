c4e6f020200990989b7ea4d9df156b23cae7431c
Rusty cat <tsukiroku@yahoo.com>
2022-04-18 17:13:32 +0900

    Update tool.ts

--- src/tool.ts
+++ src/tool.ts
@@ -1,6 +1,8 @@
 /**
- * `type Repeat<N extends number, T>`
- * > `T = 0`
+ * @description Repeat T, N times
+ * @param N number
+ * @param T any = 0
+ * 
  * ## Example: 
  * ```ts
  * type repeat1 = Repeat<3, "a">; // ["a", "a", "a"]
@@ -12,8 +14,10 @@ type Repeat<N extends number, T = 0, V extends Array<T> = []> =
         ? V : Repeat<N, T, [...V, T]>;
 
 /**
- * `type Filter<A, B>`
- * > `T = 0`
+ * @description Filter interface T
+ * @param T any
+ * @param U any
+ * 
  * ## Example: 
  * ```ts
  * interface Abcd { 
@@ -26,9 +30,9 @@ type Repeat<N extends number, T = 0, V extends Array<T> = []> =
  * type filter = Filter<Abcd, number | Array<boolean>>; // filter = { d: number, a: number, c: Array<boolean> }
  * ```
  */
-type Filter<A, B> = Pick<
-  A,
-  { [K in keyof A]: A[K] extends B ? K : never }[keyof A]
+type Filter<T, U> = Pick<
+  T,
+  { [K in keyof T]: T[K] extends U ? K : never }[keyof T]
 >;
 
 export type { Repeat, Filter };
