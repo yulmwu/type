4119951f97951c01b8a160172fa98d0d817d7ec0
Rusty cat <tsukiroku@yahoo.com>
2022-04-18 17:13:29 +0900

    Update string.ts

--- src/string.ts
+++ src/string.ts
@@ -1,17 +1,19 @@
 /**
- * `type Split<T extends string, S extends string>`
- * > `S = ""`
+ * @description Split T into U
+ * @param T string
+ * @param U string = ""
+ * 
  * ## Example: 
  * ```ts
  * type split1 = Split<"hello world">; // ["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"]
  * type split2 = Split<"hello world", " ">; // ["hello", "world"]
  * ```
  */
-type Split<T extends string, S extends string = ""> =
-  T extends S
+type Split<T extends string, U extends string = ""> =
+  T extends U
     ? []
-    : T extends `${infer A}${S}${infer B}`
-      ? [A, ...Split<B, S>]
+    : T extends `${infer A}${U}${infer B}`
+      ? [A, ...Split<B, U>]
       : [T];
 
 export type { Split };
