696448ba80b8450ef95a6ee9f7bc94581a828402
Rusty cat <tsukiroku@yahoo.com>
2022-04-22 23:56:46 +0900

    Update string.ts

--- src/string.ts
+++ src/string.ts
@@ -1,3 +1,5 @@
+import { Add, Sub, _Less } from "./math";
+
 /**
  * @description Split T into U
  * @param T string
@@ -16,4 +18,32 @@ type Split<T extends string, U extends string = ""> =
             ? [A, ...Split<B, U>]
             : [T];
 
-export type { Split };
+/**
+ * @description Join T
+ * @param T Array<string>
+ * @param U string = " "
+ * 
+ * ## Example: 
+ * ```ts
+ * type join1 = Join<["a", "b", "c"]>; // a b c
+ * type join2 = Join<["a", "b", "c"], ", ">; // a, b, c
+ * ```
+ */
+type Join<T extends Array<string>, U extends string = " ", 
+            R extends string = '', I extends number = 0> =
+    _Less<I, Sub<T['length'], 1>> extends true
+        ? Join<T, U, `${R}${T[I]}${U}`, Add<I, 1>>
+        : `${R}${T[Sub<T['length'], 1>]}`;
+
+/**
+ * @description Concat T
+ * @param T Array<string>
+ * 
+ * ## Example: 
+ * ```ts
+ * type concat = Concat<["a", "b", "c"]>; // abc
+ * ```
+ */
+type Concat<T extends Array<string>> = Join<T, "">;
+
+export type { Split, Join, Concat };
