716ce3f4c8efbfae805f24acb6835106dd324534
Rusty cat <tsukiroku@yahoo.com>
2022-04-22 23:56:39 +0900

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,5 @@
-import { Split } from "./string";
+import { Split, Join, Concat } from "./string";
 import { Add, Sub, Mul, Div, Pow, Modulo, Sqrt } from "./math";
 import { Repeat, Filter } from "./tool";
 
-declare export { Add, Sub, Mul, Div, Pow, Split, Repeat, Filter, Modulo, Sqrt };
+declare export { Add, Sub, Mul, Div, Pow, Split, Repeat, Filter, Modulo, Sqrt, Join, Concat };
