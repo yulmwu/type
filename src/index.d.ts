8b1c974c858e088e89921dafcd01bd661e21007b
tsukiroku <tsukiroku@yahoo.com>
2022-05-04 19:59:10 +0900

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,5 @@
 import { Split, Join, Concat } from "./string";
 import { Add, Sub, Mul, Div, Pow, Modulo, Sqrt } from "./math";
-import { Repeat, Filter } from "./tool";
+import { Repeat, Filter } from "./util";
 
 declare export { Add, Sub, Mul, Div, Pow, Split, Repeat, Filter, Modulo, Sqrt, Join, Concat };
